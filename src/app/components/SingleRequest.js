2eb9faa1b37e360a3b16974aa547b389f8394510
Mina Ehab Khalil <minaehab161.me@gmail.com>
2021-12-10 22:46:43 +0200

    request wait

--- src/app/components/SingleRequest.js
+++ src/app/components/SingleRequest.js
@@ -5,15 +5,15 @@ export const SingleRequest = ({ request }) => {
   return (
     <div className="request p-2">
       <div className="d-flex justify-content-between">
-        <h4>Switching Request of {request.appNo}</h4>
+        <h4>Switching Request of {request?.appNo}</h4>
       </div>
       <div>
-        <p>Major: {request.major}</p>
-        <p>Semester: {request.semester}</p>
-        <p>Tutorial Number: {request.tutNo}</p>
-        <p>Wants to go to: {request.goTo.toString()}</p>
-        <p>German: {request.germanLevel}</p>
-        <p>English: {request.englishLevel}</p>
+        <p>Major: {request?.major}</p>
+        <p>Semester: {request?.semester}</p>
+        <p>Tutorial Number: {request?.tutNo}</p>
+        <p>Wants to go to: {request?.goTo.toString()}</p>
+        <p>German: {request?.germanLevel}</p>
+        <p>English: {request?.englishLevel}</p>
       </div>
       <p></p>
     </div>
